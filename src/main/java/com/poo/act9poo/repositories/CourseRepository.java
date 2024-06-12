c916e20553623d51493ea6738a5a82791c89dbb3
Ignacio Franco <116125780+ignaciofranco2003@users.noreply.github.com>
2024-06-12 16:43:32 -0300

    Delete src/main/java/com/poo/act9poo directory

--- src/main/java/com/poo/act9poo/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poo.act9poo.repositories;
-
-import com.poo.act9poo.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, String> {
-}
